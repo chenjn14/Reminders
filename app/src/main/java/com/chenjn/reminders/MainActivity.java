bbab4b5728433419db3fbafae33eb3f376b44440
chenjn14 <chenjianan18@ict.ac.cn>
2019-07-08 18:05:29 +0800

    init

--- app/src/main/java/com/chenjn/reminders/MainActivity.java
+++ app/src/main/java/com/chenjn/reminders/MainActivity.java
@@ -3,12 +3,24 @@ package com.chenjn.reminders;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.reminders_list_view);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                R.layout.reminders_row,
+                R.id.row_text,
+                new String[]{"first record", "second record", "third record"}
+        );
+        mListView.setAdapter(arrayAdapter);
     }
 }
