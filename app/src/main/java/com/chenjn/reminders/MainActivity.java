d721260c94e4a4a799249a3778bc8d3076809e91
chenjn14 <chenjianan18@ict.ac.cn>
2019-07-08 09:50:32 +0800

    init

---
+++ app/src/main/java/com/chenjn/reminders/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chenjn.reminders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
